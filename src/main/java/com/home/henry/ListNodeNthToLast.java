3d9d4841b4277934dd4e4246b09a7583f4cb10b9
Henry <hongyu_98982@163.com>
2018-03-02 14:23:32 +0800

    Add comment on nthToLast

--- src/main/java/com/home/henry/ListNodeNthToLast.java
+++ src/main/java/com/home/henry/ListNodeNthToLast.java
@@ -1,8 +1,11 @@
 package com.home.henry;
 
+/**
+ *  find the nth to last element of a singly linked list.
+ */
 public class ListNodeNthToLast {
 
-    ListNode nthToLast(ListNode head, int n){
+    ListNode nthToLast(ListNode head, int n) {
         ListNode p1 = head;
         ListNode p2 = head;
         for (int i = 0; i < n; i++) {
@@ -11,7 +14,7 @@ public class ListNodeNthToLast {
             }
             p1 = p1.next;
         }
-        while(p1 != null){
+        while (p1 != null) {
             p1 = p1.next;
             p2 = p2.next;
         }
