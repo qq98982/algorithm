40f32999fb6fbc88cfc4dd675b829f18daeff33c
Henry <hongyu_98982@163.com>
2018-05-26 14:08:11 +0800

    Fix a bug, if index is min, it will not sort it right

--- src/main/java/com/home/henry/SelectionSort.java
+++ src/main/java/com/home/henry/SelectionSort.java
@@ -4,9 +4,9 @@ public class SelectionSort {
     public static void sortBackArray(int[] a) {
         int size = a.length;
         int i, j, max, temp;
-        for (i = 0; i < size - 1; i++) {
+        for (i = 0; i < size; i++) {
             max = 0;
-            for (j = 1; j < size - 1 - i; j++) {
+            for (j = 1; j < size - i; j++) {
                 if (a[j] > a[max]) {
                     max = j;
                 }
@@ -18,7 +18,7 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
-        int[] a = { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+        int[] a = { 0, 9, 1, 8, 2, 7, 3, 6, 4, 5 };
         sortBackArray(a);
         StringBuilder sb = new StringBuilder();
         for (int result : a) {
